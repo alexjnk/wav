2b633a39ef3898a91b4c08e95d4fc1089f5583ba
alexjnk <44424058+alexjnk@users.noreply.github.com>
2019-11-22 06:05:00 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
 function preload() {
+	intervalCurrentPosition(positionPing, 5000)
+	locationData = getCurrentPosition();
+	startingPoint = getCurrentPosition();
   soundFormats('wav');
+    keydoor = loadSound('assets/keydoor.wav');
   eastbound = loadSound('assets/eastboundtrain.wav');
+  brighton = loadSound('assets/brightonbeach.wav');
+  greatromances = loadSound('assets/texas.wav');
+  weirdbubbles = loadSound('assets/weirdbubbles1.wav');
+
 }
 
 function setup() {
-  eastbound.setVolume(0.1);
-  eastbound.play();
+	keydoor.play();
+	print(startingPoint);
+
+}
+
+function positionPing(position){
+	print(position);
+}
+
+function touchStarted() {
+  getAudioContext().resume()
 }
